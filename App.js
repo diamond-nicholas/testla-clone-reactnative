9fa9578fa1150a3c1561698de0c7bb49de4cd6d9
diamond-nicholas <diamondnicholas154@gmail.com>
2022-08-11 18:37:03 +0100

    create basic template component

--- App.js
+++ App.js
@@ -1,25 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ImageBackground,
+} from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <View style={styles.carContainer}>
+        <ImageBackground
+          source={require('./assets/images/ModelS.jpeg')}
+          style={styles.image}
+        />
         <View style={styles.titles}>
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subtitle}>Starting at $69,420</Text>
         </View>
       </View>
       <StatusBar style='auto' />
-    </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   carContainer: {
     height: '100%',
     width: '100%',
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
   },
   titles: {
     alignItems: 'center',
---
+++ Components/CarItem/styles.js
@@ -0,0 +1,5 @@
+import { StyleSheet } from 'react-native-web';
+
+const styles = StyleSheet.create({});
+
+export default styles;
