4bfa4bffd0a510fc4bb213026abfb3ae41cccbc5
diamond-nicholas <diamondnicholas154@gmail.com>
2022-08-12 07:46:11 +0100

    update button component

--- Components/StyledButton/index.js
+++ Components/StyledButton/index.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import React from 'react';
+import styles from './styles';
 
 const StyledButton = (props) => {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          console.warn('hey there');
+        }}
+      >
+        <Text style={styles.text}>Custom Order</Text>
+      </Pressable>
     </View>
   );
 };
