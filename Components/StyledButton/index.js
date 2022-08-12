6afff1edff89372881bc5e8592d66488dc4dc490
diamond-nicholas <diamondnicholas154@gmail.com>
2022-08-12 07:33:28 +0100

    add button component templates

---
+++ Components/StyledButton/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const StyledButton = (props) => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default StyledButton;
